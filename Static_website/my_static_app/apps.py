d6b85365d9c4e3d425b6c4c1aa7d3d87250a8ed9
KINGNURA007 <arunrajamani190@gamil.com>
2026-08-15 14:51:12 +0530

    initial commit

--- Static_website/my_static_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyStaticAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_static_app'
--- Static_website/my_static_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'my_static_app'
-
-urlpatterns = [
-    path('', views.index, name="index")
-    path('admin/', admin.site.urls),
-]
